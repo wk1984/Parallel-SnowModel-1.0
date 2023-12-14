cb5ca45089ec5004cbcba69c7d6205b0367724aa
Kang Wang <Kang.Wang@colorado.edu>
2023-12-14 15:09:33 +0800

    Update asc_topo_veg_to_grads.py

--- utils/asc_topo_veg_to_grads.py
+++ utils/asc_topo_veg_to_grads.py
@@ -4,26 +4,34 @@ import numpy as np
 # from xgrads import CtlDescriptor,open_CtlDataset
 import struct
 import os
+import sys
+# import matplotlib.pyplot as plt
 
 if len(sys.argv)==4:
 	topo_file   =  sys.argv[1]
-	veg_values  =  sys.argv[2]
+	veg_file    =  sys.argv[2]
 	gdat_file   =  sys.argv[3]
+	print(sys.argv)
+    
 else:
 	raise ValueError('Use this script like: python asc_to_grads.py Basin_DEM.asc Basin_VEG.asc topo_veg.gdat')
 
 topo_values = np.loadtxt(topo_file, skiprows=6)
 veg_values  = np.loadtxt(veg_file , skiprows=6)
 
-n_x = topo_values.shape[0]
-n_y = topo_values.shape[1]
+n_y = topo_values.shape[0]
+n_x = topo_values.shape[1]
 
 # gdat_file = 'test.gdat'
 ctrl_file = os.path.basename(gdat_file).split('.')[0]+'.ctl'
 
+d0 = np.flipud(topo_values)
+
+# print(d0.shape, n_x, n_y)
+
 with open(gdat_file, 'wb') as file:
     
-    for ix in np.fliplr(topo_values.transpose()).flatten():
+    for ix in d0.flatten():
     
         binary_data = struct.pack("f", ix)
     
@@ -50,10 +58,12 @@ fid.write('ENDVARS' + '\n')
 fid.close()
 
 # ctl = open_CtlDataset(ctrl_file)
-# 
+
 # # print(ctl)
 # a = ctl['topo'][0,:,:]
+# # print(a.shape)
 # a.plot()
-# 
-# b = ctl['vege'][0,:,:]
-# b.plot()
+# plt.show()
+
+# # b = ctl['vege'][0,:,:]
+# # b.plot()
